eb3b19a9eb81918983243f85a5818ce2d43e1501
Dequan Zhang <dequanzhang@bytedance.com>
2025-12-26 18:54:59 +0800

    fix build for vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ensure workers are built as ES modules to support code-splitting
+  worker: {
+    format: 'es',
+  },
 })
